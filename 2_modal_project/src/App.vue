7bf2b1d475bc1b11b2b899a9078f5a061975f038
florian autricque <fautricque@gmail.com>
2024-09-10 22:13:43 +0200

    challenge

--- 2_modal_project/src/App.vue
+++ 2_modal_project/src/App.vue
@@ -3,11 +3,39 @@
     <h1>{{ title }}</h1>
     <div v-if="showModal">
       <!-- here @close is the event trigger from the child comp with emit -->
-      <Modal :header="header" :text="text" theme="sale" @close="toggleModal" />
+      <!-- <Modal :header="header" :text="text" theme="sale" @close="toggleModal" /> -->
+      <Modal theme="sale" @close="toggleModal">
+        <!-- v-slot here is used when we have a specific structure and we want to pass something in out child comp outside the <slot> -->
+
+        <template v-slot:modal1>
+          <h1>Sign up</h1>
+          <p>For fun</p>
+        </template>
+
+        <template v-slot:links>
+          <a href="#">Sign up now</a>
+          <a href="#">More infos</a>
+        </template>
+      </Modal>
+    </div>
+
+    <div v-if="showModal2">
+      <Modal @close="toggleModalTwo">
+        <template v-slot:modal2>
+          <h1>Sign up to the news later</h1>
+          <p>For updates and promo codes</p>
+        </template>
+
+        <template v-slot:links>
+          <a href="#">Sign up now</a>
+          <a href="#">More infos</a>
+        </template>
+      </Modal>
     </div>
     <p>Welcome...</p>
     <!-- shift here is a modifier. To modify the event listener -->
     <button @click.shift="toggleModal">Show modal (shift)</button>
+    <button @click="toggleModalTwo">Show modal 2</button>
 
     <!-- HOW TO USE REFS -->
     <!-- <input type="text" ref="name" />
@@ -23,9 +51,10 @@ export default {
   data() {
     return {
       title: "My first Vue app",
-      header: "Sign up for the Giveaway!",
-      text: "Sign up",
+      // header: "Sign up for the Giveaway!",
+      // text: "Sign up",
       showModal: false,
+      showModal2: false,
     };
   },
   components: { Modal },
@@ -39,6 +68,9 @@ export default {
     // this.$refs.name.classList.add("active");
     // this.$refs.name.focus();
     // },
+    toggleModalTwo() {
+      this.showModal2 = !this.showModal2;
+    },
   },
 };
 </script>
--- 2_modal_project/src/components/Modal.vue
+++ 2_modal_project/src/components/Modal.vue
@@ -2,15 +2,24 @@
   <!-- self make the modal close only when we click on the div backdrop.  -->
   <div class="backdrop" @click.self="closeModal">
     <div class="modal" :class="{ sale: theme === 'sale' }">
-      <h1>{{ header }}</h1>
-      <p>{{ text }}</p>
+      <!-- <h1>{{ header }}</h1>
+      <p>{{ text }}</p> -->
+
+      <!-- HERE slot is use to pass something that can be big such as a form -->
+      <slot name="modal1"></slot>
+      <slot></slot>
+      <slot name="modal2"></slot>
+      <div class="actions">
+        <slot name="links"></slot>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  props: ["header", "text", "theme"],
+  // props: ["header", "text", "theme"],
+  props: ["theme"],
   methods: {
     closeModal() {
       //emit triggers an event from a child component (modal) to its parent comp
@@ -48,6 +57,34 @@ export default {
   color: white;
 }
 .modal.sale.h1 {
+  color: white;
+}
+.modal .actions {
+  text-align: center;
+  margin: 30px 0 10px 0;
+  color: #333;
+}
+.modal .actions a {
+  color: #333;
+  padding: 8px;
+  border: 1px solid #eee;
+  border-radius: 4px;
+  text-decoration: none;
+  margin: 10px;
+}
+
+/* sale styles */
+.modal.sale {
+  background: crimson;
+  color: white;
+}
+.modal.sale h1 {
+  color: white;
+}
+.modal.sale .actions {
+  color: white;
+}
+.modal.sale .actions a {
   color: white;
 }
 </style>
