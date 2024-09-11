6131c798a154afc0e6d247ab874625724c414d31
florian autricque <fautricque@gmail.com>
2024-09-11 16:19:14 +0200

    end project

--- 3_reaction_timer/src/components/Results.vue
+++ 3_reaction_timer/src/components/Results.vue
@@ -1,11 +1,34 @@
 <template>
-  <p>Reaction time - {{ score }} ms</p>
+  <div>
+    <p>Reaction time - {{ score }} ms</p>
+    <p class="rank">{{ rank }}</p>
+  </div>
 </template>
 
 <script>
 export default {
   props: ["score"],
+  data() {
+    return {
+      rank: null,
+    };
+  },
+  mounted() {
+    if (this.score < 250) {
+      this.rank = "Ninja fingers";
+    } else if (this.score < 400) {
+      this.rank = "Rapid reflexes";
+    } else {
+      this.rank = "Snail pace ...";
+    }
+  },
 };
 </script>
 
-<style></style>
+<style>
+.rank {
+  font-size: 1.4em;
+  color: #0faf87;
+  font-weight: bold;
+}
+</style>
