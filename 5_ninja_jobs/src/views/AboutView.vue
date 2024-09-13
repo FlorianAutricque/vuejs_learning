356e3b5766fcb8260bac92094e997b9a86c28450
florian autricque <fautricque@gmail.com>
2024-09-13 13:33:40 +0200

    cleanup

--- 5_ninja_jobs/src/views/AboutView.vue
+++ 5_ninja_jobs/src/views/AboutView.vue
@@ -1,5 +1,32 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt, harum
+      tempora? Laboriosam, neque hic maiores dolorum nostrum aperiam facere
+      molestias iusto dolores temporibus id atque error nisi harum quasi nobis?
+      Ducimus ipsa aut, sed vel error fuga, obcaecati pariatur ullam nihil
+      soluta reprehenderit laudantium placeat dolorum nemo esse voluptate. Totam
+      vero facere harum nulla voluptates quam qui doloremque quae ratione atque
+      suscipit, assumenda dolorum! Modi recusandae harum incidunt, unde esse
+      omnis nisi? Maiores asperiores assumenda recusandae quam. Tenetur
+      excepturi eius inventore commodi non illum voluptatem, nulla, pariatur
+      consectetur minima nobis animi fugiat modi itaque architecto voluptas in
+      aperiam eligendi dicta.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat vero qui
+      repudiandae pariatur adipisci corrupti! Laudantium soluta architecto,
+      commodi excepturi dolores doloremque at velit placeat minima fuga porro,
+      quia ullam quos dicta. Blanditiis eveniet doloremque, itaque rem non
+      maiores illum fuga exercitationem, omnis perferendis earum? Alias dicta
+      maiores reiciendis. Adipisci placeat vel laborum repellendus, officiis
+      quis exercitationem debitis temporibus excepturi dolore architecto ad
+      iure. Quidem tenetur, est tempore, delectus pariatur mollitia magni
+      consectetur temporibus voluptatem neque facilis exercitationem beatae
+      repudiandae minus aperiam consequuntur ipsa laudantium ab doloremque cum,
+      molestiae nulla voluptatibus in quisquam? Dolore consequuntur veniam ea
+      voluptatum quia deleniti!
+    </p>
   </div>
 </template>
--- 5_ninja_jobs/src/views/HomeView.vue
+++ 5_ninja_jobs/src/views/HomeView.vue
@@ -1,18 +1,38 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Hoomepage</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa fugit velit
+      voluptate magnam blanditiis, nobis repellendus modi nemo maiores dolore
+      nulla nostrum esse ullam deserunt alias, eligendi ex ea iusto? Pariatur
+      amet temporibus iste expedita neque! Qui quos exercitationem eligendi
+      autem blanditiis fugit quasi cumque aliquam sequi ea perferendis
+      recusandae nobis, quas praesentium architecto nihil nisi quis facilis
+      temporibus laborum accusantium reprehenderit id delectus! Nam quo maxime
+      unde qui, placeat autem modi mollitia, odio deserunt sint at quidem ipsa
+      nemo sed voluptas eos ullam excepturi deleniti. Itaque quibusdam porro
+      eveniet ea, debitis id, error, fugit reiciendis mollitia laborum libero
+      magni.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa fugit velit
+      voluptate magnam blanditiis, nobis repellendus modi nemo maiores dolore
+      nulla nostrum esse ullam deserunt alias, eligendi ex ea iusto? Pariatur
+      amet temporibus iste expedita neque! Qui quos exercitationem eligendi
+      autem blanditiis fugit quasi cumque aliquam sequi ea perferendis
+      recusandae nobis, quas praesentium architecto nihil nisi quis facilis
+      temporibus laborum accusantium reprehenderit id delectus! Nam quo maxime
+      unde qui, placeat autem modi mollitia, odio deserunt sint at quidem ipsa
+      nemo sed voluptas eos ullam excepturi deleniti. Itaque quibusdam porro
+      eveniet ea, debitis id, error, fugit reiciendis mollitia laborum libero
+      magni.
+    </p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  name: "HomeView",
+  components: {},
+};
 </script>
