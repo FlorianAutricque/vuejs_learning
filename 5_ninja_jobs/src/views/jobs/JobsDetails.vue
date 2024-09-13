7af18df69d99a32c7aedc77b00443a2a56c40811
florian autricque <fautricque@gmail.com>
2024-09-13 15:12:19 +0200

    fetching data

--- 5_ninja_jobs/src/views/jobs/JobsDetails.vue
+++ 5_ninja_jobs/src/views/jobs/JobsDetails.vue
@@ -1,14 +1,29 @@
 <template>
-  <div>
-    <h1>Jobs details</h1>
+  <div v-if="job">
+    <h1>{{ job.title }}</h1>
     <!-- This will give the :id that is in the url -->
     <p>The job id is: {{ id }}</p>
+    <p>The job detail: {{ job.details }}</p>
+  </div>
+  <div v-else>
+    <p>Lodaing job details...</p>
   </div>
 </template>
 
 <script>
 export default {
   props: ["id"],
+  data() {
+    return {
+      job: null,
+    };
+  },
+  mounted() {
+    fetch("http://localhost:3000/jobs/" + this.id)
+      .then((res) => res.json())
+      .then((data) => (this.job = data))
+      .catch((err) => console.log(err.message));
+  },
   // data() {
   //   return {
   //     id: this.$route.params.id,
