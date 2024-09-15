4f5b136eb52044281626792d3ccea960ba5e96dd
florian autricque <fautricque@gmail.com>
2024-09-15 12:32:58 +0200

    filter

--- 6_project-planner/src/App.vue
+++ 6_project-planner/src/App.vue
@@ -1,11 +1,16 @@
 <template>
   <div>
+    <Navbar />
     <router-view />
   </div>
 </template>
 
 <script>
-export default {};
+import Navbar from "./components/Navbar.vue";
+
+export default {
+  components: { Navbar },
+};
 </script>
 <style>
 body {
---
+++ 6_project-planner/src/views/EditProject.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <h1>Edit project</h1>
+    <form @submit.prevent="handleUpdate">
+      <label>Title:</label>
+      <input type="text" required v-model="title" />
+      <label>Details:</label>
+      <textarea required v-model="details"></textarea>
+      <button>Update Project</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["id"],
+  data() {
+    return {
+      title: "",
+      details: "",
+      uri: "http://localhost:3000/projects/" + this.id,
+    };
+  },
+  mounted() {
+    fetch(this.uri)
+      .then((res) => res.json())
+      .then((data) => {
+        (this.title = data.title), (this.details = data.details);
+      });
+  },
+  methods: {
+    handleUpdate() {
+      fetch(this.uri, {
+        method: "PATCH",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          title: this.title,
+          details: this.details,
+        }),
+      })
+        .then(() => {
+          this.$router.push("/");
+        })
+        .catch((err) => console.log(err.message));
+    },
+  },
+};
+</script>
+
+<style></style>
