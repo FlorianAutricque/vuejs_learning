823e8f8a4a6eb0b29ffd579e9331bb9f37ab1a25
florian autricque <fautricque@gmail.com>
2024-09-23 14:42:56 +0200

    ref vs reactive

--- 7_composition_api/src/views/HomeView.vue
+++ 7_composition_api/src/views/HomeView.vue
@@ -1,29 +1,52 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <p>My name is {{ name }} and my age is {{ age }}</p>
-    <button @click="handleClick">Click me</button>
-    <button @click="age++">Add age</button>
+    <!-- <h2>FIRST STEP USING REF</h2> -->
+    <!-- <p>My name is {{ name }} and my age is {{ age }}</p> -->
+    <!-- <button @click="handleClick">Click me</button>
+    <button @click="age++">Add age</button> -->
+    <!-- <input type="text" v-model="name" /> -->
 
-    <input type="text" v-model="name" />
+    <h2>REF</h2>
+    <p>{{ ninjaOne.name }} - {{ ninjaOne.age }}</p>
+    <button @click="updateNinjaOne">update ninja one</button>
+
+    <h2>REACTIVE</h2>
+    <p>{{ ninjaTwo.name }} - {{ ninjaTwo.age }}</p>
+    <button @click="updateNinjaTwo">update ninja two</button>
   </div>
 </template>
 
 <script>
-import { ref } from "vue";
+import { ref, reactive } from "vue";
 export default {
   name: "HomeView",
   setup() {
     //this are not reactive variables (whitout ref), they wont update in the template like when we were using in the data() function
 
-    const name = ref("flo");
-    const age = ref(30);
+    //FIRST STEP LEARNING REF
+    // const name = ref("flo");
+    // const age = ref(30);
 
-    const handleClick = () => {
-      name.value = "luigi";
+    // const handleClick = () => {
+    //   name.value = "luigi";
+    // };
+
+    // return { name, age, handleClick };
+
+    //REF VS REACTIVE
+    const ninjaOne = ref({ name: "mario", age: 30 });
+    const ninjaTwo = reactive({ name: "luigi", age: 35 });
+
+    const updateNinjaOne = () => {
+      ninjaOne.value.age = 40;
     };
 
-    return { name, age, handleClick };
+    const updateNinjaTwo = () => {
+      ninjaTwo.age = 40;
+    };
+
+    return { ninjaOne, updateNinjaOne, ninjaTwo, updateNinjaTwo };
   },
 };
 </script>
