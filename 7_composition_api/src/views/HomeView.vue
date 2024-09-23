830249ec4ff5b05f413e35e63c0befdd7a441b5e
florian autricque <fautricque@gmail.com>
2024-09-23 14:53:00 +0200

    computed

--- 7_composition_api/src/views/HomeView.vue
+++ 7_composition_api/src/views/HomeView.vue
@@ -7,18 +7,23 @@
     <button @click="age++">Add age</button> -->
     <!-- <input type="text" v-model="name" /> -->
 
-    <h2>REF</h2>
+    <!-- <h2>REF</h2>
     <p>{{ ninjaOne.name }} - {{ ninjaOne.age }}</p>
     <button @click="updateNinjaOne">update ninja one</button>
 
     <h2>REACTIVE</h2>
     <p>{{ ninjaTwo.name }} - {{ ninjaTwo.age }}</p>
-    <button @click="updateNinjaTwo">update ninja two</button>
+    <button @click="updateNinjaTwo">update ninja two</button> -->
+
+    <h2>COMPUTED</h2>
+    <input type="text" v-model="search" />
+    <p>search term = {{ search }}</p>
+    <div v-for="name in matchingNames" :key="name">{{ name }}</div>
   </div>
 </template>
 
 <script>
-import { ref, reactive } from "vue";
+import { ref, reactive, computed } from "vue";
 export default {
   name: "HomeView",
   setup() {
@@ -35,18 +40,28 @@ export default {
     // return { name, age, handleClick };
 
     //REF VS REACTIVE
-    const ninjaOne = ref({ name: "mario", age: 30 });
-    const ninjaTwo = reactive({ name: "luigi", age: 35 });
+    // const ninjaOne = ref({ name: "mario", age: 30 });
+    // const ninjaTwo = reactive({ name: "luigi", age: 35 });
 
-    const updateNinjaOne = () => {
-      ninjaOne.value.age = 40;
-    };
+    // const updateNinjaOne = () => {
+    //   ninjaOne.value.age = 40;
+    // };
 
-    const updateNinjaTwo = () => {
-      ninjaTwo.age = 40;
-    };
+    // const updateNinjaTwo = () => {
+    //   ninjaTwo.age = 40;
+    // };
 
-    return { ninjaOne, updateNinjaOne, ninjaTwo, updateNinjaTwo };
+    // return { ninjaOne, updateNinjaOne, ninjaTwo, updateNinjaTwo };
+
+    //COMPUTED
+    const search = ref("");
+    const names = ref(["mario", "luigi", "toad", "bowser", "koopa", "peach"]);
+
+    const matchingNames = computed(() => {
+      return names.value.filter((name) => name.includes(search.value));
+    });
+
+    return { names, search, matchingNames };
   },
 };
 </script>
